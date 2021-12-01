4c9a999b635b062d9d1425a266125f7c0bc0a0f5
randall yeo <randall.yeo@isobar.com>
2021-12-01 11:42:31 +0800

    removed credentials for database

--- database.js
+++ database.js
@@ -2,7 +2,7 @@ const mysql = require('mysql2');
 
 module.exports = mysql.createConnection({
     host:'localhost',
-    user:'root',
-    password:'yeotwig1999',
+    user:'',
+    password:'',
     database: 'carpark'
 });
